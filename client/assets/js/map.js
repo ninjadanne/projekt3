41b6b3b7371788c776c4048b41412577a1243556
Fredrik Hall <per.fredrik.hall@gmail.com>
2015-04-21 18:00:54 +0200

    Markers are now showing on the map

--- client/assets/js/map.js
+++ client/assets/js/map.js
@@ -1,7 +1,7 @@
 (function() {
 
     var mapModule = angular
-    .module('googleMaps', ['uiGmapgoogle-maps'])
+    .module('googleMaps', ['uiGmapgoogle-maps', 'placeService'])
     .config(function(uiGmapGoogleMapApiProvider) {
         uiGmapGoogleMapApiProvider.configure({
             key: 'AIzaSyBovPRmUSEAduOe4SspxqSt-TngHRdRFNA',
@@ -9,21 +9,38 @@
             libraries: 'weather,geometry,visualization'
         });
     })
-    .controller("mapController", function($scope, uiGmapGoogleMapApi) {
+    .controller("mapController", function($scope, $http, uiGmapGoogleMapApi) {
         // Centrera till Stapelbäddsparken
-        $scope.map = { center: { latitude: 55.613565, longitude: 12.983973 }, zoom: 12 };
-        // Sätt ut en markör
-        $scope.marker = {
-            id: 0,
-            coords: {
-                latitude: 55.613565,
-                longitude: 12.983973
+        $scope.map = { center: { latitude: 55.613565, longitude: 12.983973 }, zoom: 12, bounds: {} };
+
+        $http.get('http://manu.fhall.se/p3/place')
+        .success(function(data, status, headers, config) {
+            // Sätt ut en markör
+            var markers = [];
+
+            $scope.$watch('markers', function() {
+                $scope.markers = markers;
+                console.log($scope.markers);
+            });
+
+            for (var i = 0; i < data.length; i++) {
+                markers.push({
+                    id: data[i].id,
+                    idKey: data[i].id,
+                    title: data[i].name,
+                    latitude: data[i].lat,
+                    longitude: data[i].lon
+                });
             }
-        };
+
+            $scope.markers = markers;
+        })
+        .error(function(data, status, headers, config) {
+            console.log('error!');
+        });
 
         // uiGmapGoogleMapApi is a promise
         uiGmapGoogleMapApi.then(function(maps) {
-
         });
     });
 })();
