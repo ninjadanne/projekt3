632f9eb12995d8ad0d4a7215168f137f1b1ab1c3
Fredrik Hall <per.fredrik.hall@gmail.com>
2015-04-21 20:30:37 +0200

    Added functionality for retrieving the users position if the unit and client supports it

--- client/assets/js/map.js
+++ client/assets/js/map.js
@@ -9,15 +9,47 @@
         });
     })
     .controller("mapController", function($scope, $http, uiGmapGoogleMapApi, placeService) {
-        // Centrera till Stapelbäddsparken
-        $scope.map = { center: { latitude: 55.613565, longitude: 12.983973 }, zoom: 12, bounds: {} };
+        // Standard startposition (Stapelbäddsparken, Malmö)
+        var userPosition = { latitude: 55.613565, longitude: 12.983973 };
+
+        // Initiera karta
+        $scope.map = { center: { latitude: userPosition.latitude, longitude: userPosition.longitude }, zoom: 12, bounds: {} };
+
+        // Funktion för att centrera kartan
+        function centerMap(position) {
+            $scope.map.center = position;
+            $scope.map.redraw = true;
+        }
+
+        // Om enheten och klienten stöder geolocation
+        if (navigator.geolocation) {
+            // Hämta användarens position
+            navigator.geolocation.getCurrentPosition(function(location) {
+                userPosition = {
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                };
+                console.log('The users position is: ');
+                console.log(userPosition);
+                centerMap(userPosition);
+            });
+            // Övervaka användarens position
+            navigator.geolocation.watchPosition(function(location) {
+                userPosition = {
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude
+                };
+                centerMap(userPosition);
+            });
+        }
 
         // Skapa en array med markers
         var markers = [];
 
-        $scope.$watch('markers', function() {
-            $scope.markers = markers;
-        });
+        // Övervaka markörerna
+        // $scope.$watch('markers', function() {
+        //     $scope.markers = markers;
+        // });
 
         // Hämta platser från placeService
         placeService.getPlaces().then(function(data) {
