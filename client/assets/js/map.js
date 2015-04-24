0b1d30739e2b2c91aa9fc397caa7c7b545fafc21
Fredrik Hall <per.fredrik.hall@gmail.com>
2015-04-24 15:32:00 +0200

    Changes in placeService and map

--- client/assets/js/map.js
+++ client/assets/js/map.js
@@ -1,39 +1,40 @@
-(function() {
+/** Skate map module */
+var skateMap = angular.module('skate.Map', ['uiGmapgoogle-maps']);
 
-    angular.module('skate.Map', ['uiGmapgoogle-maps'])
-    .config(function(uiGmapGoogleMapApiProvider) {
-        uiGmapGoogleMapApiProvider.configure({
-            key: 'AIzaSyBovPRmUSEAduOe4SspxqSt-TngHRdRFNA',
-            v: '3.17',
-            libraries: 'weather,geometry,visualization'
-        });
-    })
-    .controller("mapController", function($scope, $http, uiGmapGoogleMapApi, placeService) {
-
-        // Hämta användarens nuvarande position
-        placeService.getCurrentPosition().then(function(userPosition) {
-            // Initiera karta
-            $scope.map = { center: { latitude: userPosition.latitude, longitude: userPosition.longitude }, zoom: 12, bounds: {} };
-        });
-
-        // Övervaka användarens position
-        // if (navigator.geolocation) {
-        //     navigator.geolocation.watchPosition(function(location) {
-        //         userPosition = {
-        //             latitude: location.coords.latitude,
-        //             longitude: location.coords.longitude
-        //         };
-        //         $scope.map.center = userPosition;
-        //         $scope.map.refresh = true;
-
-        //         console.log(userPosition);
-        //     });
-        // }
-
-        // Hämta platser från placeService
-        placeService.getPlaces().then(function(data) {
-            $scope.markers = data; // Sätt hämtad data som markörer
-        });
-
+/** Skate map config */
+skateMap.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyBovPRmUSEAduOe4SspxqSt-TngHRdRFNA',
+        v: '3.17',
+        libraries: 'weather,geometry,visualization'
     });
-})();
+})
+
+/** Skate map controllers */
+skateMap.controller("mapController", function($scope, $http, uiGmapGoogleMapApi, placeService) {
+
+    // Hämta användarens nuvarande position
+    placeService.getCurrentPosition().then(function(userPosition) {
+        // Initiera karta
+        $scope.map = { center: { latitude: userPosition.latitude, longitude: userPosition.longitude }, zoom: 12, bounds: {} };
+        // Hämta platser från placeService
+    });
+
+    placeService.getPlaces().then(function(data) {
+        $scope.markers = data; // Sätt hämtad data som markörer
+    });
+
+    // Övervaka användarens position
+    // if (navigator.geolocation) {
+    //     navigator.geolocation.watchPosition(function(location) {
+    //         userPosition = {
+    //             latitude: location.coords.latitude,
+    //             longitude: location.coords.longitude
+    //         };
+    //         $scope.map.center = userPosition;
+    //         $scope.map.refresh = true;
+
+    //         console.log(userPosition);
+    //     });
+    // }
+});
--- client/assets/js/place.js
+++ client/assets/js/place.js
@@ -1,64 +1,81 @@
-(function() {
-    var placeApp = angular.module('skate.Place', []);
+var placeApp = angular.module('skate.Place', []);
 
-    placeApp.factory('placeService', function($http, $q) {
-        return {
-            getPlaces: function() {
-                endPoint = 'http://manu.fhall.se/p3b/';
-                lat = '55.5919247';
-                lon = '13.0105638';
-                var dfr = $q.defer();
-                var places;
-                $http.get(endPoint + 'place_by_coor.php', {'lat': lat, 'lng': lon}).success(function(data) {
-                    places = [];
-                    data = data.data;
+/** Service */
+placeApp.factory('placeService', function($http, $q) {
+    /**
+     * Convert a place response from backend
+     * @param  {[type]} place [description]
+     * @return {[type]}       [description]
+     */
+    function convertPlace(place) {
+        var convertedPlace = {
+            title: place.Name,
+            latitude: place.Latitude,
+            longitude: place.Longitude,
+            tags: place.Activity.split(' '), // The tags are space separated
+            description: place.Description,
+            rating: Math.round(place.Rating * 10) / 10, // Round the rating to 1 decimal
+            images: [
+                {
+                    uri: place.Pic_URI
+                }
+            ]
+        };
+        return convertedPlace;
+    }
 
-                    dfr.resolve(places);
+    /**
+     * Get the users current position
+     * @return {[type]} [description]
+     */
+    function getCurrentPosition() {
+        var dfr = $q.defer();
+        // Standard startposition (Stapelbäddsparken, Malmö)
+        var userPosition = { latitude: 55.613565, longitude: 12.983973 };
 
-                    for (var i = 0; i < data.length; i++) { // Loopa igenom alla hämtade platser
-                        places.push({
-                            id: i,
-                            idKey: i,
-                            title: data[i].Name,
-                            latitude: data[i].Latitude,
-                            longitude: data[i].Longitude,
-                            tags: data[i].Activity.split(' '), // The tags are space separated
-                            description: data[i].Description,
-                            rating: Math.round(data[i].Rating * 10) / 10, // Round the rating to 1 decimal
-                            images: [
-                                {
-                                    uri: data[i].Pic_URI
-                                }
-                            ]
-                        });
-                    }
-                });
-                return dfr.promise; // Return a promise
-            },
-            getCurrentPosition: function() {
-                var dfr = $q.defer();
-                // Standard startposition (Stapelbäddsparken, Malmö)
-                var userPosition = { latitude: 55.613565, longitude: 12.983973 };
+        // Om enheten och klienten stöder geolocation
+        if (navigator.geolocation) {
+            // Hämta användarens position
+            navigator.geolocation.getCurrentPosition(function(location) {
+                userPosition = {
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                };
+                dfr.resolve(userPosition);
+            });
+        }
 
-                // Om enheten och klienten stöder geolocation
-                if (navigator.geolocation) {
-                    // Hämta användarens position
-                    navigator.geolocation.getCurrentPosition(function(location) {
-                        userPosition = {
-                            latitude: location.coords.latitude,
-                            longitude: location.coords.longitude,
-                        };
-                        dfr.resolve(userPosition);
-                    });
+        return dfr.promise; // Return a promise
+    }
+
+    return {
+        getPlaces: function(coords) {
+            coords = coords ? coords : getCurrentPosition(); // If coords is null get current position
+            endPoint = 'http://manu.fhall.se/p3b/place_by_coor.php';
+            var dfr = $q.defer();
+            var places = [];
+            $http.get(endPoint, {'lat': coords.latitude, 'lng': coords.longitude}).success(function(data) {
+                data = data.data;
+
+                for (var i = 0; i < data.length; i++) { // Loopa igenom alla hämtade platser
+                    place = convertPlace(data[i]);
+                    place.id = i;
+                    places.push(place);
                 }
 
-                return dfr.promise; // Return a promise
-            }
-        };
+                dfr.resolve(places); // Return places
+            });
+            return dfr.promise; // Return a promise
+        },
+        getCurrentPosition: function() {
+            return getCurrentPosition();
+        }
+    };
+});
+
+/** Controllers */
+placeApp.controller('getPlaces', ['$scope', '$http', 'placeService', function($scope, $http, placeService) {
+    placeService.getPlaces().then(function(places) {
+        $scope.places = places;
     });
-    placeApp.controller('getPlaces', ['$scope', '$http', 'placeService', function($scope, $http, placeService) {
-        placeService.getPlaces().then(function(data) {
-            $scope.places = data;
-        });
-    }]);
-})();
+}]);
